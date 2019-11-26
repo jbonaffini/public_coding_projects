63c71e729b77d4b86d8bea4bcfe74cf1ff766d17
James Bonaffini <james.bonaffini@gmail.com>
2019-11-25 19:24:21 -0500

    update to main m file for 6310 including spline function, figure recreations

--- BME6310_Project/src/Fibrin_Thrombin_main.m
+++ BME6310_Project/src/Fibrin_Thrombin_main.m
@@ -3,9 +3,9 @@
 
 % Starting values
 %    [TF*			Xa			IXa			XIa		
-%		Fibrin		ES			yS			IIa]
+%		Fibrin			ES			yS			IIa]
 c0 = [1*10^-6		0.17*10^-6	0.09*10^-6	.031*10^-6 ...
-		18*10^-6		0*10^-6		0.09*10^-6		1.4*10^-6];
+		18*10^-6		0*10^-6		0.0*10^-6		1.4*10^-6];
 
 tspan = 0:1:850;
 
@@ -15,14 +15,14 @@ p.k_i = log(2) / 60;
 p.k_elute = log(2) / 2;
 p.n = [1 1 1 0.18 0.05 0.36 1];
     p.a = [0.48 0.32 5.53 24.7 58.8 4.98*10^-5 0.065];
-p.E_O_total = 1.6; p.Ek_f = 280; p.Ek_r = 280;
-p.y_O_total = 0.3; p.yk_f = 10; p.yk_r = 10;
+p.E_O_total = 1.6; p.Ek_f = 100; p.Ek_r = 280;
+p.y_O_total = 0.3; p.yk_f = 100; p.yk_r = 15;
 
 % Parameters - Extrinsic & Intrisic (XIa=0, a(6)=a(7)=0)
 pEI = p;
     pEI.a = [0.48 0.32 5.53 24.7 58.8 0 0];
 
-% Parameters - Extrinsic Only (XIa=IXa=0, a(2)=0)
+% Parameters - Extrinsic Only (XIa=IXa=0, a(2)=a(3)=0)
 pE = p;
     pE.a = [0.48 0 0 24.7 58.8 4.98*10^-5 0.065];
 
@@ -61,7 +61,7 @@ legend('boxoff')
 
 % Thrombin
 subplot(223); plot(t,c(:,6)+c(:,7),'b-',...
-	t,c(:,6)+c(:,7)-cEI(:,6)+cEI(:,7),'r-',...
+	t,c(:,6)+c(:,7)-cEI(:,6)-cEI(:,7),'r-',...
 	t,cEI(:,6)+cEI(:,7),'y-',...
 	t,(cE(:,6)+cE(:,7))*1000,'m--','LineWidth', 2); 
 xlabel('time(sec)'); ylabel ('IIa [\muM]');
@@ -77,7 +77,7 @@ subplot(224); plot(t,c(:,6)+c(:,7),'b-',...
 	t,c(:,6),'y-',...
 	t,c(:,8)*10,'m--','LineWidth', 2)
 xlabel('time(sec)'); ylabel ('IIa');
-title('Thrombin'); axis tight;
+title('Thrombin'); axis([0 1000 0 20]);
 legend('Total', 'High affinity-bound', ...
 	'Weak-bound', 'Free IIa (x 10)',...
 	'FontSize',8,'Location','best');
@@ -87,3 +87,48 @@ legend('boxoff')
 
 % figure; plot(t,c(:,6),t,c(:,7), t,c(:,8)*10);
 % legend('Weak Bonding', 'Strong Bonding', 'IIa');
+
+%%
+mse_spline (tspan, c(:,3));
+function [] = mse_spline  (origx, origy)
+	
+	test = 50:50:400;
+	mse = zeros(1,length(test));
+	
+	for i = 1 : length(mse)
+		mse(i) = ds_spline (origx, origy, test(i));
+	end
+	
+	figure;
+	plot(test,mse)
+end
+
+
+function [mse] = ds_spline (origx, origy, ds)
+	dsx = downsample(origx,ds);
+	dsy = downsample(origy,ds);
+	
+	pts = round(length(origx) / ds);
+	idx = round(linspace(1,length(origx), pts));
+	dsx=[]; dsy=[];
+	for i = 1 : length(idx)
+		dsx = [dsx origx(idx(i))];
+		dsy = [dsy origy(idx(i))];
+	end
+	
+	pp = spline(dsx, dsy, origx);
+	
+	mse = mean( ((pp - origy')./pp).^ 2);
+	
+	figure;
+	plot(origx,origy,'b-',...
+	dsx,dsy,'r-',...
+	origx,pp,'k-',...
+	'LineWidth', 2); 
+	xlabel('x'); ylabel ('y');
+	title('');
+	legend('Original', 'Downsampled',...
+	['Spline Interp: ',num2str(ds)],...
+		'FontSize',8,'Location','best');
+	legend('boxoff')
+end
